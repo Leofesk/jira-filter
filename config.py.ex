b34cfc206a2a3564bcc81e5ac350f4e9d872e9ec
Mikhail Gorelov <leofesk@gmail.com>
2021-07-18 19:00:19 +0300

    Added work with filter ids

--- config.py.ex
+++ config.py.ex
@@ -1,1 +1,14 @@
 # Example config file
+
+url = "https://{SERVER_HOST}/rest/api/{API_VERSION}/filter"
+
+headers = {
+    "Accept": "application/json",
+    "Content-Type": "application/json"
+}
+
+username = 'username'
+password = 'password'
+
+filter_ids = [1, 2, 3, 4, 5]
+max_results = 999
--- run.py
+++ run.py
@@ -1,1 +1,21 @@
-print("Jira Filter")
+import requests
+import config
+
+
+def get_jql_filter(filter_id):
+    url = config.url + "/" + str(filter_id)
+    response = requests.get(url, headers=config.headers, auth=(config.username, config.password)).json()
+    print("Filter name: " + response['name'])
+    return response['searchUrl'] + "&maxResults=" + str(config.max_results)
+
+
+def get_issues(jql_url):
+    return requests.get(jql_url, headers=config.headers, auth=(config.username, config.password)).json()
+
+
+def filter_total(filter_response):
+    print("Filter total: " + str(filter_response['total']))
+
+
+for filterId in config.filter_ids:
+    filter_total(get_issues(get_jql_filter(filterId)))
